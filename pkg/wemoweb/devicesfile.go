17b63dc3fba98489e5da9037cf8926ad344904c1
Suresh Randawa <srandawa@gmail.com>
2020-07-02 12:37:36 -0400

    Changed from json to yaml config file

--- pkg/wemoweb/devicesfile.go
+++ pkg/wemoweb/devicesfile.go
@@ -11,23 +11,24 @@ import (
 func ReadDevices(config Config_t) (map[string]map[string]string, error) {
     devicesYaml, err := ioutil.ReadFile(config.DevicesFile)
     if err != nil {
-        return map[string]map[string]string{}, err
+        return nil, err
     }
 
-    var deviceList map[string]map[string]string
+    var deviceList map[string]map[string]map[string]string
     err = yaml.Unmarshal(devicesYaml, &deviceList)
-    return deviceList, nil
+    return deviceList["devices"], nil
 }
 
 
 
 func WriteDevices(config Config_t, deviceList map[string]map[string]string) error {
-    yamlOut, err := yaml.Marshal(deviceList)
+    top := map[string]map[string]map[string]string{"devices": deviceList}
+    yamlOut, err := yaml.Marshal(top)
     if err != nil {
         return err
     }
 
-    err = ioutil.WriteFile(config.DevicesFile, yamlOut, 0644)
+    err = ioutil.WriteFile(config.DevicesFile, append([]byte("---\n"), yamlOut...), 0644)
     if err != nil {
         return err
     }
@@ -41,11 +42,10 @@ func UpdateDevices(config Config_t, deviceList, newDevices map[string]map[string
     for k, v := range newDevices {
         _, ok := deviceList[k]
         if !ok {
-            deviceList[k] = map[string]string{"Host": v["Host"]}
+            deviceList[k] = v
             changed = true
-        } else if v["Host"] != deviceList[k]["Host"] {
-            log.Printf("Found updated device: %s old:%s new: %s\n", k, deviceList[k], v)
-            //deviceList[k]["Host"] = v["Host"]
+        } else if v["Host"] != deviceList[k]["Host"] || v["FriendlyName"] != deviceList[k]["FriendlyName"] {
+            log.Printf("Found updated device: %s\n", k)
             deviceList[k] = v
             changed = true
         }
